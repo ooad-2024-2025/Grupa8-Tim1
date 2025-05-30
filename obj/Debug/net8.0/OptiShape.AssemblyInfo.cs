be998e9e0f465a212401f80cdb07c59d03e6930d
tlolic1 <tlolic1@etf.unsa.ba>
2025-05-30 12:12:25 +0200

    Popravljen UI

--- obj/Debug/net8.0/OptiShape.AssemblyInfo.cs
+++ obj/Debug/net8.0/OptiShape.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptiShape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ca1ad2945a363d3d9680c1c86a33bd22e9f326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511152b2494659684efdec33fbf7135ffd34ed9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptiShape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptiShape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
